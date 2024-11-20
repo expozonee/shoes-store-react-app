f02bb683d30086be160c7d881c006428bac1f560
abrahem haj ali <boss.abrahem@hotmail.com>
2024-11-20 13:55:28 +0200

    created new function to handle adding the product to the cart or remove it

--- src/components/ProductCard/ProductCard.tsx
+++ src/components/ProductCard/ProductCard.tsx
@@ -1,5 +1,8 @@
-import { Link } from "react-router-dom";
 import "./ProductCard.css";
+import { Link } from "react-router-dom";
+import { useUser } from "../../provider/UserProvider";
+import { useStore } from "../../provider/StoreProvider";
+import { ShoeData } from "../../types/ShoeData";
 
 type ProductCardProps = {
   id: string;
@@ -7,6 +10,8 @@ type ProductCardProps = {
   brand: string;
   title: string;
   price: number;
+  slug: string;
+  gender: string;
 };
 
 export default function ProductCard({
@@ -14,21 +19,62 @@ export default function ProductCard({
   imageURL,
   brand,
   price,
-  title,
+  title: name,
+  ...rest
 }: ProductCardProps) {
+  const { isAdmin } = useUser();
+  const { removeItemFromStore, addToCart, cartItems, removeItemFromCart } =
+    useStore();
+
+  function handleCartAdd(isInCart: boolean) {
+    if (isInCart) {
+      removeItemFromCart(id);
+    } else {
+      const productToAdd = {
+        id,
+        imageURL,
+        brand,
+        price,
+        name,
+        ...rest,
+      };
+
+      addToCart(productToAdd);
+    }
+  }
+
   return (
     <div className="product-card">
       <div className="image__container">
         <img src={imageURL} alt="Shoe Image" className="product-card__image" />
       </div>
       <div className="product__details">
-        <h2 className="product-card__title">{title}</h2>
+        <h2 className="product-card__title">{name}</h2>
         <p className="product-card__brand">Brand: {brand}</p>
         <p className="product-card__price">Price: ${price}</p>
         <Link to={`/store/product/${id}`}>
           <button className="product-card__button">View Product</button>
         </Link>
+        <button
+          onClick={() => {
+            handleCartAdd(cartItems.some((i: ShoeData) => i.id === id));
+          }}
+        >
+          {cartItems.some((i: ShoeData) => i.id === id)
+            ? "Remove from cart"
+            : "Add to cart"}
+        </button>
       </div>
+      {isAdmin && (
+        <button
+          style={{ backgroundColor: "red" }}
+          onClick={() => {
+            removeItemFromStore(id);
+          }}
+        >
+          Remove
+        </button>
+      )}
     </div>
   );
 }
