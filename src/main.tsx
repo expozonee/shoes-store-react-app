a2202ad2caa379be227e02e6a8af0fe4159fb2e1
abrahem haj ali <boss.abrahem@hotmail.com>
2024-11-20 12:13:39 +0200

    added the providers that was created to the app

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
+import "./index.css";
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-// import App from "./App.tsx";
-
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import HomePage from "./pages/HomePage/HomePage.tsx";
 import Store from "./pages/Store/Store.tsx";
+import HomePage from "./pages/HomePage/HomePage.tsx";
+import UserProvider from "./provider/UserProvider.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import StoreProducts, {
   storeLoader,
 } from "./components/StoreProducts/StoreProducts.tsx";
 import ProductPage, {
   productLoader,
 } from "./pages/ProductPage/ProductPage.tsx";
+import StoreProvider from "./provider/StoreProvider.tsx";
 
 const router = createBrowserRouter([
   {
@@ -38,7 +38,10 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    {/* <App /> */}
-    <RouterProvider router={router} />
+    <UserProvider>
+      <StoreProvider>
+        <RouterProvider router={router} />
+      </StoreProvider>
+    </UserProvider>
   </StrictMode>
 );
