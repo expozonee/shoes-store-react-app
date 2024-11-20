8da3185139abae644b0ebf03af034c13a224c964
abrahem haj ali <boss.abrahem@hotmail.com>
2024-11-20 12:16:03 +0200

    added the check if the user is signed in to either diaplay the login form or not

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,8 +1,11 @@
 import { Link } from "react-router-dom";
 import "./HomePage.css";
 import SignInForm from "../../components/SignInForm/SignInForm";
+import { useUser } from "../../provider/UserProvider";
 
 export default function HomePage() {
+  const { isSignedIn } = useUser();
+
   return (
     <div className="main__container">
       <div className="welcome__container">
@@ -11,7 +14,7 @@ export default function HomePage() {
           <button> Start Shopping!</button>
         </Link>
         <hr />
-        <SignInForm />
+        {isSignedIn ? null : <SignInForm />}
       </div>
     </div>
   );
