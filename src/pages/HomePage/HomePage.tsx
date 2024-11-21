f597f8bf8dc5f2d121f7987c7d47e01a4d764eaa
abrahem haj ali <boss.abrahem@hotmail.com>
2024-11-21 16:34:33 +0200

    changed the design in the homepage

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,5 @@
-import { Link } from "react-router-dom";
 import "./HomePage.css";
+import { Link } from "react-router-dom";
 import SignInForm from "../../components/SignInForm/SignInForm";
 import { useUser } from "../../provider/UserProvider";
 
@@ -9,12 +9,23 @@ export default function HomePage() {
   return (
     <div className="main__container">
       <div className="welcome__container">
+        <div className="image__container">
+          <img
+            src="https://images.unsplash.com/photo-1491553895911-0055eca6402d?q=80&w=1760&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+            alt=""
+          />
+        </div>
         <h1>Welcome to Shoes store</h1>
         <Link to={"/store"}>
           <button> Start Shopping!</button>
         </Link>
-        <hr />
-        {isSignedIn ? null : <SignInForm />}
+
+        {isSignedIn ? null : (
+          <>
+            <hr />
+            <SignInForm />
+          </>
+        )}
       </div>
     </div>
   );
