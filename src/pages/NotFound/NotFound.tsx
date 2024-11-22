4e19862fb390a4bcf18608f3f4c6ed40d7157d64
abrahem haj ali <boss.abrahem@hotmail.com>
2024-11-22 08:29:35 +0200

    added the actual error to the not found page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,11 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
 import "./NotFound.css";
 
 export default function NotFound() {
+  const error = useRouteError();
+
   return (
     <div className="not-found">
       <h1>404 - Page Not Found</h1>
       <p>Sorry, the page you are looking for does not exist.</p>
+      <p>Error: {`${error}`}</p>
       <Link to={"/"}>
         <button>Go back to the homepage</button>
       </Link>
